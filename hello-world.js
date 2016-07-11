e513cc7f0c9606f9854243822709680454456913
Dylan Pelletier <dylan.pelletier1979@gmail.com>
2016-07-11 17:31:54 +0000

    Finished exercise 2

--- hello-world.js
+++ hello-world.js
@@ -1,9 +1,20 @@
 // This is the first exercise
 
-console.log("This is the first line of the code now wait for it...");
+// console.log("This is the first line of the code now wait for it...");
 
-setTimeout(function(){
-    console.log("but this is what you've been waiting for! Yippee!");
-}, 10000);
+// setTimeout(function(){
+//     console.log("but this is what you've been waiting for! Yippee!");
+// }, 10000);
 
-console.log('This is not what you are waiting for...');
+// console.log('This is not what you are waiting for...');
+
+// Exercise 2
+
+function adNauseum(){
+    setTimeout(function(){
+        console.log('Hello World!');
+    }, 10000);
+    adNauseum();
+};
+
+adNauseum();
