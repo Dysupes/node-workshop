4826954ba0224e88bef32acedac1c41521aebbed
Dylan Pelletier <dylan.pelletier1979@gmail.com>
2016-07-11 17:20:59 +0000

    Finished the first exercise

---
+++ hello-world.js
@@ -0,0 +1,9 @@
+// This is the first exercise
+
+console.log("This is the first line of the code now wait for it...");
+
+setTimeout(function(){
+    console.log("but this is what you've been waiting for! Yippee!");
+}, 10000);
+
+console.log('This is not what you are waiting for...');
